b9c5ae4a12d3706b3ab0dbccf61d943c66061813
silentorb <christopherwjohnson@gmail.com>
2015-02-28 09:21:29 -0700

    Finally moved the MetaHub project into a subfolder.  Breaking apart the test project as well.

--- Test/Utility.cs
+++ Test/Utility.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using imperative;
+using imperative.expressions;
 using metahub.jackolantern;
 using metahub.logic;
 using metahub.logic.schema;
@@ -27,20 +28,20 @@ namespace test
             return reader.ReadToEnd();
         }
 
-        public static JackOLantern create_jack()
-        {
-            var json = Utility.load_resource("test.meta.resources.schema.json");
-            var hub = new metahub.Hub();
-            hub.load_parser();
-            var space = new Namespace("test", "test");
-            hub.load_schema_from_string(json, space);
-            var overlord = new Overlord();
-//            var railway = new Railway(hub, "cpp");
-            var target = new Mock_Target(overlord);
-            var logician = new Logician(hub.schema);
-//            var jack = new JackOLantern(logician, overlord, railway, target);
-//            return jack;
-            return null;
-        }
+        //public static JackOLantern create_jack()
+        //{
+        //    var json = Utility.load_resource("test.meta.resources.schema.json");
+        //    var hub = new metahub.Hub();
+        //    hub.load_parser();
+        //    var space = new Namespace("test", "test");
+        //    hub.load_schema_from_string(json, space);
+        //    var overlord = new Overlord();
+        //    //            var railway = new Railway(hub, "cpp");
+        //    var target = new Mock_Target(overlord);
+        //    var logician = new Logician(hub.schema);
+        //    //            var jack = new JackOLantern(logician, overlord, railway, target);
+        //    //            return jack;
+        //    return null;
+        //}
     }
 }

e24905c90bac740919a3f47ce9c2c454618ba66e
silentorb <christopherwjohnson@gmail.com>
2015-03-09 12:51:28 -0600

    Added project for a new lexer and parser.

---
+++ Interpreter/lexer/Lexer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using parser;
+
+namespace interpreter.lexer
+{
+    public class Lexer
+    {
+        private Definition definition;
+
+        public Lexer()
+        {
+            Definition boot_definition = new Definition();
+            boot_definition.load_parser_schema();
+            Bootstrap context = new Bootstrap(boot_definition);
+
+            var result = context.parse(load_resource("lexer.grammar"), boot_definition.patterns[0], false);
+            //Debug_Info.output(result);
+            if (!result.success)
+                throw new Exception("Could not load grammar.");
+
+            var match = (Match)result;
+
+            definition = new Definition();
+            definition.load(match.get_data().dictionary);
+        }
+
+        public static string load_resource(string filename)
+        {
+            var path = "interpreter.resources." + filename;
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var stream = assembly.GetManifestResourceStream(path);
+            if (stream == null)
+                throw new Exception("Could not find file " + path + ".");
+
+            var reader = new StreamReader(stream);
+            return reader.ReadToEnd().Replace("\r\n", "\n");
+        }
+    }
+}
